e31d3822599e252fad22f700724f34c4075a8c70
Alex <tgnwest@gmail.com>
2020-04-20 18:39:19 +0300

    Add App icons, launch screen, row height

--- CoolPlaces/MainViewController.swift
+++ CoolPlaces/MainViewController.swift
@@ -29,10 +29,18 @@ class MainViewController: UITableViewController {
 
         cell.textLabel?.text = places[indexPath.row]
         cell.imageView?.image = UIImage(named: places[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
 
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+    
     /*
     // MARK: - Navigation
 
