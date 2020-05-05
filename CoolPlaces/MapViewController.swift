0c22903ca1bf636cd1686f4ea5b07f7c6b6f4800
Alex <tgnwest@gmail.com>
2020-05-05 07:31:42 +0300

    added map vc

---
+++ CoolPlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  CoolPlaces
+//
+//  Created by Alex Tregubov on 05.05.2020.
+//  Copyright © 2020 Alex Tregubov. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
